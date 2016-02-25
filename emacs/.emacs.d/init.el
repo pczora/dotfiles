cb5195c6d1c202fb48ee95c906e121adba63e4b8
Philipp Czora <pczora@googlemail.com>
2016-02-25 09:14:57 +0100

    Some adjustments to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,5 +5,26 @@
 ;; Show line numbers
 (global-linum-mode t)
 
-;; Use M-SPC instead of C-SPC for setting marks
+;; Highlight current line
+(global-hl-line-mode t)
+
+;; Use M-SPC instead of C-SPC for setting marks (since I already use C-SPC as tmux' prefix)
 (global-set-key (kbd "M-SPC") (lambda() (interactive) (push-mark nil nil 1)))
+
+;; Set theme
+(load-theme 'smyx t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "603a9c7f3ca3253cb68584cb26c408afcf4e674d7db86badcfe649dd3c538656" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
