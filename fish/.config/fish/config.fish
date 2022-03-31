cc605723eeac2125216bc4dd3ec1b4ae0ab4cbaa
Philipp Czora <philipp@czora.dev>
2022-03-31 08:12:20 +0200

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# pyenv setup
+status --is-interactive; and . (pyenv init - | psub)
+
+thefuck --alias | source
+
+switch (uname) 
+  case Darwin
+    fish_add_path /usr/local/opt/curl/bin
+end
+
+# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
+set -gx PATH $PATH $HOME/.krew/bin
