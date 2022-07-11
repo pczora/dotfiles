6d8eb969f5bcc8b838ac7c7f6b1ef76b8c1eb1bc
Philipp Czora <philipp@czora.dev>
2022-07-11 14:27:13 +0200

    Add WSL2 specific fish/X config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,12 +20,20 @@ alias klf='kubectl logs -f'
 alias cat='bat'
 alias ls='exa'
 
-switch (uname) 
-  case Darwin
+switch (uname -a) 
+  case "*Darwin*"
     fish_add_path /usr/local/opt/curl/bin
+case "*microsoft*"
+  set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+  set -gx LIBGL_ALWAYS_INDIRECT 1
+  set -gx XDG_RUNTIME_DIR /home/pcz/repos/
+  set -gx RUNLEVEL 3
+
+  alias rust-analyzer='rustup run nightly rust-analyzer'
 end
 
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
-set -gx PATH $PATH $HOME/.krew/bin
+fish_add_path $HOME/.krew/bin
+fish_add_path $HOME/.cargo/bin
