1eebbd8b2b211948a6c85ebe7faad1373ccf4a0f
Philipp Czora <philipp@czora.dev>
2022-10-10 08:35:50 +0200

    Fix symbols-outline config

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -155,6 +155,7 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
 require'nvim-treesitter.configs'.setup{}
 require("luasnip.loaders.from_vscode").lazy_load()
+require("symbols-outline").setup()
 vim.g.symbols_outline = {
   auto_preview = false
 }
