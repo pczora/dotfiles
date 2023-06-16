6cb884242ff243e42496757d421f7a42ed723173
Philipp Czora <philipp@czora.dev>
2023-06-16 08:22:32 +0200

    Fix fish prompt

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,6 @@
 function fish_right_prompt
   set num_contexts (kubectl config get-contexts | wc -l)
   if type -q kubectl && test (kubectl config get-contexts | wc -l) -gt 1
-    set_color green; echo "ﴱ "; set_color normal; kubectl config current-context; 
+    set_color green; echo "⎈"; set_color normal; kubectl config current-context; 
  end
 end
